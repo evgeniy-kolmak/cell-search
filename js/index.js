eaddd2fd94e944c629384d3b16f0c857113dd6c8
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2022-06-12 15:21:44 +0300

    full remake app and add new features

--- js/index.js
+++ js/index.js
@@ -4,15 +4,27 @@ const columnInRow = Array.from(row).map(el => Array.from(el.children));
 const column = document.querySelectorAll('td');
 const start = document.querySelector('.start');
 const reset = document.querySelectorAll('.reset');
-const timer = document.querySelector('.timer');
+const modal = document.querySelector('.modal')
 const content = document.querySelector('.content');
 const addTime = document.querySelector('.add-time');
 const record = document.querySelector('.record');
-const modal = document.querySelector('.modal');
-let countTimer = 60000;
+const timer = document.querySelector('.timer');
+let values;
+let doneValues;
+let logs;
 let timerId;
-const bonus = ['star', 'bomb', 'luck'];
+let countTimer = 60000;
+const interval = 1000;
+const bonus = ['star', 'bomb', 'luck', 'skull'];
+let star;
+let bomb;
+let luck;
+let skull;
+let bonusValues = [];
+let statusGame;
 
+
+// Добовление ID к ячейкам таблицы
 const addIdElements = () => {
   Array.from(row).map((element, index) => element.id = index)
   columnInRow.map((items, i) => {
@@ -21,6 +33,55 @@ const addIdElements = () => {
 }
 
 addIdElements();
+
+// Выбор рандомно 10 ячеек
+const randomCell = count => {
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    const value = Math.round(Math.random() * 100).toString();
+    if (!result.includes(value)) {
+      if (value.length > 2) {
+        i--;
+        continue;
+      } else if (value.length < 2) {
+        result.push(`0${value}`)
+      } else {
+        result.push(value)
+      }
+    } else {
+      i--;
+      continue;
+    }
+  }
+
+  return result
+}
+
+// Финиш
+const isFinish = value => {
+  if (!doneValues.includes(value)) {
+    doneValues.push(value)
+    if (doneValues.length === 10) {
+      let count = countTimer / interval;
+      clearInterval(timerId)
+      modal.style.display = 'block'
+      timer.textContent = 'Вы победили!'
+      isRecord(count);
+      content.textContent = `Ваше время ${count} секунд(ы)`;
+      document.querySelector('.cup').style.display = 'block';
+    }
+  }
+}
+
+// Рекорд
+const isRecord = count => {
+  if (count > localStorage.timeCount) {
+    localStorage.timeCount = count;
+  }
+
+  record.textContent = `Ваш рекорд ${localStorage.timeCount} секунд(ы)`;
+
+}
 
 const showRecord = () => {
   if (localStorage.timeCount === undefined) {
@@ -31,121 +92,51 @@ const showRecord = () => {
 
 showRecord();
 
-start.addEventListener('click', function (e) {
-  e.preventDefault();
-  table.style.backgroundColor = "#fff"
-  table.style.transitionDuration = '1s'
+// Количество бонусов
+const countBonus = value => {
+  if (value === 'star') {
+    return Math.round(Math.random() * 5);
+  } else if (value === 'bomb') {
+    return Math.round(Math.random() * 4);
+  } else if (value === 'skull') {
+    return Math.round(Math.random());
+  } else {
+    return 1;
+  }
+}
 
-  const randomCell = count => {
-    const result = [];
-    for (let i = 0; i < count; i++) {
-      const value = Math.round(Math.random() * 100).toString();
+// Добовление бонусов
+const addBonus = coll => {
+  const collectionCount = coll.map(item => (countBonus(item)));
+  const sumCountBonus = collectionCount.reduce((acc, item) => acc += item, 0)
+  for (let i = 0; i < sumCountBonus; i++) {
+    const value = Math.round(Math.random() * 100).toString();
+    if (values.includes(value) || bonusValues.includes(value)) {
+      i--;
+      continue;
+    } else if (!values.includes(value)) {
       if (value.length > 2) {
-        result.push('00')
-      } else if (value.length < 2) {
-        result.push(`0${value}`)
-      } else if (result.includes(value)) {
-        i--
-        continue;
-      } else {
-        result.push(value)
-      }
-    }
-
-    return result
-  }
-
-
-  const countCell = 10;
-  const values = randomCell(countCell);
-  // console.log(values);
-
-  const data = [];
-
-  const isFinish = data => {
-    let count = 0;
-    if (data.length === countCell) {
-      count = countTimer / interval;
-      clearInterval(timerId)
-      modal.style.display = 'block'
-      timer.textContent = 'Вы победили!'
-      isRecord(count);
-      content.textContent = `Ваше время ${count} секунд(ы)`;
-      document.querySelector('.cup').style.display = 'block';
-
-    }
-  }
-
-  const bonusValues = [];
-
-  const addBonus = (coll) => {
-    const collectionCount = coll.map(item => (countBonus(item)));
-    const sumCountBonus = collectionCount.reduce((acc, item) => acc += item, 0)
-    for (let i = 0; i < sumCountBonus; i++) {
-      const value = Math.round(Math.random() * 100).toString();
-      if (values.includes(value) || bonusValues.includes(value)) {
         i--;
         continue;
-      } else if (!values.includes(value)) {
-        if (value.length > 2) {
-          bonusValues.push('00')
-        } else if (value.length < 2) {
-          bonusValues.push(`0${value}`)
-        } else {
-          bonusValues.push(value)
-        }
+      } else if (value.length < 2) {
+        bonusValues.push(`0${value}`)
+      } else {
+        bonusValues.push(value)
       }
     }
-    return collectionCount;
-
   }
+  return collectionCount;
 
-  const countBonusValues = addBonus(bonus);
-  console.log(bonusValues);
-  console.log(countBonusValues);
-
-
-  const star = [];
-
-  for (let i = 0; i < countBonusValues[0]; i++) {
-    star.push(bonusValues[i])
-  }
-
-  const bomb = [];
-
-  for (let i = countBonusValues[0]; i < bonusValues.length - 1; i++) {
-    bomb.push(bonusValues[i])
-  }
-
-  const luck = bonusValues.slice(-1)
+}
 
 
-
-
-
-  // вставлять элементы сразу в html при кнопке старт, а не при нажатии и рикпываит классом
-
-  // Дубли создания бонусов
-
-
-
-
-
-
-  const isRecord = count => {
-    if (count > localStorage.timeCount) {
-      localStorage.timeCount = count;
-    }
-
-    record.textContent = `Ваш рекорд ${localStorage.timeCount} секунд(ы)`;
-
-  }
-
-  column.forEach(el => {
-    el.addEventListener('click', function () {
-      if (values.includes(el.id)) {
-        el.style.backgroundColor = '#66CC66';
-        if (!data.includes(el.id)) {
+// Начало игры
+const startGame = statusGame => {
+  if (statusGame) {
+    table.addEventListener('click', function (e) {
+      if (values.includes(e.target.id)) {
+        e.target.style.backgroundColor = '#66CC66';
+        if (!doneValues.includes(e.target.id)) {
           addTime.style.opacity = 1;
           addTime.style.color = '#66CC66';
           addTime.textContent = '+ 10 секунд'
@@ -153,76 +144,122 @@ start.addEventListener('click', function (e) {
             addTime.style.opacity = 0;
           }, 1000)
           countTimer += 10000;
-          data.push(el.id);
-          isFinish(data)
+          isFinish(e.target.id)
         }
       } else {
-        if (!data.includes(el.id)) {
-          if (star.includes(el.id)) {
-            console.log(0);
-            el.style.backgroundColor = '#7c2222';
-            const img = document.createElement('img');
-            img.style.width = '32px';
-            img.src = './img/star.png';
-            const td = document.getElementById(`${el.id}`);
-            td.classList.add('bonus');
-            td.append(img)
-            // 
-            addTime.style.opacity = 1;
-            addTime.style.color = '#7c2222';
-            addTime.textContent = '+ 7 секунд'
-            setTimeout(() => {
-              addTime.style.opacity = 0;
-            }, 1500)
-            countTimer += 7000;
-
-          } else if (bomb.includes(el.id)) {
-            el.style.backgroundColor = 'orange';
-            const img = document.createElement('img');
-            img.style.width = '32px';
-            img.src = './img/bomb.png';
-            const td = document.getElementById(`${el.id}`);
-            td.classList.add('bonus');
-            td.append(img)
-            // 
-            addTime.style.opacity = 1;
-            addTime.style.color = 'orange';
-            addTime.textContent = 'Упс! - 3 секунды'
-            setTimeout(() => {
-              addTime.style.opacity = 0;
-            }, 1500)
-            countTimer -= 3000;
-          } else if (luck.includes(el.id)) {
-            el.style.backgroundColor = 'purple';
-            const img = document.createElement('img');
-            img.style.width = '32px';
-            img.src = './img/luck.png';
-            const td = document.getElementById(`${el.id}`);
-            td.classList.add('bonus');
-            td.append(img)
-            // 
-            addTime.style.opacity = 1;
-            addTime.style.color = 'purple';
-            addTime.textContent = 'Ого! Это большая удача'
-            setTimeout(() => {
-              addTime.style.opacity = 0;
-            }, 1500)
-          } else {
-            el.style.backgroundColor = '#FF6666';
+        if (e.target.nodeName === 'TD') {
+          e.target.style.backgroundColor = '#FF6666';
+          if (!logs.includes(e.target.id)) {
             addTime.style.opacity = 1;
             addTime.style.color = '#FF6666';
             addTime.textContent = '- 1 секунда'
             setTimeout(() => {
               addTime.style.opacity = 0;
-              countTimer -= 1000;
             }, 500)
+            countTimer -= 1000
+            logs.push(e.target.id)
+          }
+        } else if (e.target.nodeName === 'IMG') {
+          if (star.includes(e.target.parentNode.id)) {
+            e.target.style.opacity = 1;
+            e.target.parentNode.style.backgroundColor = '#7c2222';
+            if (!logs.includes(e.target.parentNode.id)) {
+              addTime.style.opacity = 1;
+              addTime.style.color = '#7c2222';
+              addTime.textContent = 'Звезда! +5 секунд'
+              setTimeout(() => {
+                addTime.style.opacity = 0;
+              }, 1000)
+              countTimer += 5000
+              logs.push(e.target.parentNode.id)
+            }
+          } else if (bomb.includes(e.target.parentNode.id)) {
+            e.target.style.opacity = 1;
+            e.target.parentNode.style.backgroundColor = 'orange';
+            if (!logs.includes(e.target.parentNode.id)) {
+              addTime.style.opacity = 1;
+              addTime.style.color = '#7c2222';
+              addTime.textContent = 'Упс! -8 секунд'
+              setTimeout(() => {
+                addTime.style.opacity = 0;
+              }, 1000)
+              countTimer -= 8000;
+              logs.push(e.target.parentNode.id);
+            }
+          } else if (luck.includes(e.target.parentNode.id)) {
+            e.target.style.opacity = 1;
+            e.target.parentNode.style.backgroundColor = 'purple';
+            if (!logs.includes(e.target.parentNode.id)) {
+              addTime.style.opacity = 1;
+              addTime.style.color = 'purple';
+              addTime.textContent = 'Ого, это большая удача!'
+              setTimeout(() => {
+                addTime.style.opacity = 0;
+              }, 2000)
+              logs.push(e.target.parentNode.id);
+              values.forEach(item => {
+                if (!doneValues.includes(item)) {
+                  const cell = document.getElementById(`${item}`);
+                  cell.style.transitionDuration = '.8s';
+                  cell.style.backgroundColor = 'grey';
+                  setTimeout(() => {
+                    if (!doneValues.includes(item)) {
+                      cell.style.backgroundColor = null;
+                    }
+                  }, 3000)
+                }
+              })
+            }
+          } else {
+            e.target.style.opacity = 1;
+            e.target.parentNode.style.backgroundColor = 'black';
+            if (!logs.includes(e.target.parentNode.id)) {
+              addTime.style.opacity = 1;
+              addTime.style.color = 'black';
+              addTime.textContent = 'Сделка с дьяволом!'
+              setTimeout(() => {
+                addTime.style.opacity = 0;
+                countTimer += 30000;
+              }, 2000)
+              logs.push(e.target.parentNode.id);
+              column.forEach(cell => {
+                if (cell.id !== e.target.parentNode.id) {
+                  cell.style.transitionDuration = '.8s';
+                  cell.style.backgroundColor = null;
+                  if (cell.firstChild) {
+                    cell.firstChild.style.opacity = 0;
+                    cell.firstChild.style.transitionDuration = '.8s';
+                  }
+                }
+              })
+            }
           }
         }
       }
     })
-  })
 
-  let interval = 1000;
+  } else {
+    return statusGame;
+  }
+}
+
+
+start.addEventListener('click', function (e) {
+  statusGame = true;
+  startGame(statusGame);
+  e.preventDefault();
+  statusGame = true;
+  table.style.backgroundColor = '#fff'
+  table.style.transitionDuration = '1s'
+
+  values = randomCell(10);
+
+  doneValues = [];
+  logs = [];
+
+  const countBonusValues = addBonus(bonus);
+
+
   timerId = setInterval(() => {
     countTimer -= interval;
     timer.textContent = `${countTimer / interval
@@ -236,9 +273,84 @@ start.addEventListener('click', function (e) {
   }, interval);
 
 
-  document.querySelectorAll('.content-promo').forEach(item => item.style.display = 'none')
+  star = [];
+  bomb = [];
+  // Добовление бонусов на поле
+  const addSuperBonus = () => {
+    let count;
 
+    if (countBonusValues[countBonusValues.length - 1] === 0) {
+      luck = bonusValues.slice(-1);
+      count = 1;
+
+    } else {
+      luck = [bonusValues[bonusValues.length - 2]];
+      skull = bonusValues.slice(-1);
+      count = 2;
+
+      skull.forEach(el => {
+        const element = document.getElementById(`${el}`);
+        const img = document.createElement('img');
+        img.style.verticalAlign = 'bottom'
+        img.style.opacity = 0;
+        img.style.width = '32px';
+        img.style.height = '32px';
+        img.src = './img/skull.png';
+        element.append(img)
+
+      })
+
+    }
+
+    for (let i = countBonusValues[0]; i < bonusValues.length - count; i++) {
+      bomb.push(bonusValues[i])
+    }
+
+    for (let i = 0; i < countBonusValues[0]; i++) {
+      star.push(bonusValues[i])
+    }
+
+
+    star.forEach(el => {
+      const element = document.getElementById(`${el}`);
+      const img = document.createElement('img');
+      img.style.verticalAlign = 'bottom';
+      img.style.opacity = 0;
+      img.style.width = '32px';
+      img.style.height = '32px';
+      img.src = './img/star.png';
+      element.append(img)
+    })
+
+    bomb.forEach(el => {
+      const element = document.getElementById(`${el}`);
+      const img = document.createElement('img');
+      img.style.verticalAlign = 'bottom';
+      img.style.opacity = 0;
+      img.style.width = '32px';
+      img.style.height = '32px';
+      img.src = './img/bomb.png';
+      element.append(img);
+    })
+
+    luck.forEach(el => {
+      const element = document.getElementById(`${el}`);
+      const img = document.createElement('img');
+      img.style.verticalAlign = 'bottom'
+      img.style.opacity = 0;
+      img.style.width = '32px';
+      img.style.height = '32px';
+      img.src = './img/luck.png';
+      element.append(img)
+
+    })
+  }
+  addSuperBonus();
+
+  document.querySelectorAll('.content-promo').forEach(item => item.style.display = 'none')
 })
+
+
 
 reset.forEach(btn => {
   btn.addEventListener('click', function (e) {
@@ -246,32 +358,25 @@ reset.forEach(btn => {
     clearInterval(timerId)
     table.style.backgroundColor = '#000';
     column.forEach(el => {
-      el.innerHTML = '';
       el.style.backgroundColor = null;
       el.style.transitionDuration = '.5s';
       el.addEventListener('click', function () {
         el.style.backgroundColor = null;
       })
+      el.innerHTML = '';
+      bonusValues = [];
+      star = [];
+      bomb = [];
+      luck = null;
+      skull = null;
+
     })
 
     clearInterval(timerId)
     countTimer = 60000;
     modal.style.display = 'none';
     timer.textContent = 'Найдите все 10 плиток'
+    statusGame = false;
 
   })
 })
-
-
-
-
-const countBonus = value => {
-  if (value === 'star') {
-    return Math.round(Math.random() * 5);
-  } else if (value === 'bomb') {
-    return Math.round(Math.random() * 4);
-  } else {
-    return 1;
-  }
-}
-
